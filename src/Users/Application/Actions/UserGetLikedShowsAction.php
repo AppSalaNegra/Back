2775efee5a6b43f2e8f9eed819be2d42e7cd17fc
Martín Ramonda <tini.ramonda@gmail.com>
2024-04-25 18:24:56 +0200

    fixed getLikedShowsAction. i must test functionality

--- src/Users/Application/Actions/UserGetLikedShowsAction.php
+++ src/Users/Application/Actions/UserGetLikedShowsAction.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Users\Application\Actions;
 
+use App\Events\Domain\EventsRepository;
+use App\Users\Domain\Exception\UserNotFound;
 use App\Users\Domain\User;
 use App\Users\Domain\UserRepository;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -12,11 +14,16 @@ use Symfony\Component\Serializer\SerializerInterface;
 final class UserGetLikedShowsAction extends UserAction
 {
     private SerializerInterface $serializer;
+    private EventsRepository $eventsRepository;
 
-    public function __construct(UserRepository $repository, SerializerInterface $serializer)
-    {
+    public function __construct(
+        UserRepository $repository,
+        EventsRepository $eventsRepository,
+        SerializerInterface $serializer
+    ) {
         parent::__construct($repository);
         $this->serializer = $serializer;
+        $this->eventsRepository = $eventsRepository;
     }
 
     protected function action(): Response
@@ -24,17 +31,21 @@ final class UserGetLikedShowsAction extends UserAction
         $data = $this->getFormData();
         $userId = $data['id'];
         $user = $this->repository->findById($userId);
+        if (null === $user) {
+            throw new UserNotFound();
+        }
         return $this->respondWithData(['userEvents' => $this->getAllUserEvents($user)]);
     }
 
     private function getAllUserEvents(User $user): array
     {
+        if (empty($user->getLikedShows())) {
+            return [];
+        }
         $events = [];
         foreach ($user->getLikedShows() as $eventId) {
-            //TODO: find event by id
-            $events[] = [
-
-            ];
+            $event = $this->eventsRepository->findById($eventId);
+            $events[] = $this->serializer->serialize($event, 'json');
         }
         return $events;
     }
